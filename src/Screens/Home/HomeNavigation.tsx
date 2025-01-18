d4c864613f9b862f140dab1ed758ac0e9c6f9f47
INJOKER11 <67804388+INJOKER11@users.noreply.github.com>
2025-01-18 21:06:08 +0100

    Comm

--- src/Screens/Home/HomeNavigation.tsx
+++ src/Screens/Home/HomeNavigation.tsx
@@ -22,6 +22,9 @@ const HomeNavigation = () => {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="ThankYou" component={ThankYou} />
       </Stack.Group>
+      <div>
+        <div></div>
+      </div>
 
       <Stack.Group
         screenOptions={{
